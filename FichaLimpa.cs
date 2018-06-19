82298b75ee4475cfd15013f90ca3ffbbebce7194
victorcappa <40408965+victorcappa@users.noreply.github.com>
2018-06-19 20:34:12 -0300

    Update FichaLimpa.cs
    Sistema de ficha limpa para cada personagem
    Sistema de perda de ficha limpa
    Método CPI, que aumenta velocidade da perda de popularidade

--- FichaLimpa.cs
+++ FichaLimpa.cs
@@ -36,7 +36,7 @@ public class FichaLimpa : MonoBehaviour {
         GanhaFicha();
         MaxMinFicha();
 
-       // PlayerPrefs.GetFloat("VeloPerdaPop", PlayerPrefs.GetFloat("VeloPerdaPop"));
+       // PlayerPrefs.GetFloat("VeloPerdaPop", PlayerPrefs.GetFloat("VeloPerdaPop")); //
     }
 
 
