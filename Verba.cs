b40210b02c8ee9bd1624966cc1b33a53f23556f1
victorcappa <40408965+victorcappa@users.noreply.github.com>
2018-06-21 13:47:03 -0300

    Update Verba - criado o  verba inicial
    criei o método verbaInicial() para desbloquear o primeiro personagem. Após comprar esse personagem, a verba vai para zero (só pode escolher um inicial)

--- Verba.cs
+++ Verba.cs
@@ -6,15 +6,20 @@ public class Verba : MonoBehaviour {
 
     public int verba;
     public Text verba_txtUI;
+    public bool primeiraVez;
 
     public void Awake()
     {
         verba = PlayerPrefs.GetInt("Verba");
         minVerba();
+        verbaInicial();
+       
     }
 
     public void Update()
-    {
+    {        
+        //PlayerPrefs.DeleteAll();
+
 
         verba = PlayerPrefs.GetInt("Verba");
         verba_txtUI.text = verba.ToString();
@@ -35,6 +40,27 @@ public class Verba : MonoBehaviour {
         if (PlayerPrefs.GetInt("Verba") <= 0)
         {
             PlayerPrefs.SetInt("Verba", 0);
+        }
+    }
+
+    public void verbaInicial()
+    {
+        if (verba <= 0 && PlayerPrefs.GetInt("LevelIndice") <= 0 && PlayerPrefs.GetInt("DentroDoPartido") == 0)
+        {
+            primeiraVez = true;
+
+            if (primeiraVez == true && PlayerPrefs.GetInt("DentroDoPartido") == 0 )
+            {
+                verba = 200;
+                PlayerPrefs.SetInt("Verba", verba);
+            }
+            if (primeiraVez == true && PlayerPrefs.GetInt("DentroDoPartido") > 0 && verba > 0 )
+            {
+                primeiraVez = false;
+                verba = 0;
+
+            }
+
         }
     }
 
