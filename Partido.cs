31355a10571bd10d2798beb41bda2c66ab143d17
victorcappa <40408965+victorcappa@users.noreply.github.com>
2018-06-22 10:30:15 -0300

    Update Partido.cs
    Problema com Suplicy resolvido (código estava com um errinho)

--- Partido.cs
+++ Partido.cs
@@ -1,11 +1,19 @@
-﻿using UnityEngine;
+using UnityEngine;
 
 public class Partido : MonoBehaviour {
     
-     GameObject[] PersonagensScene = new GameObject[20];     public GameObject LulaScene, CiroScene, BolsonaroScene;
-    public int LulaDentroScene, CiroDentroScene, BolsoDentroScene;     //public GameObject[] ArrayPoliticos;      void Awake()     {         LulaDentroScene = PlayerPrefs.GetInt("LulaDentroScene");
+     GameObject[] PersonagensScene = new GameObject[20];
+    public GameObject LulaScene, CiroScene, BolsonaroScene, DilmaScene, SuplicyScene, EneasScene;
+    public int LulaDentroScene, CiroDentroScene, BolsoDentroScene, DilmaDentroScene, SuplicyDentroScene, EneasDentroScene;
+    //public GameObject[] ArrayPoliticos;
+
+    void Awake()
+    {
+        LulaDentroScene = PlayerPrefs.GetInt("LulaDentroScene");
         CiroDentroScene = PlayerPrefs.GetInt("CiroDentroScene");
         BolsoDentroScene = PlayerPrefs.GetInt("BolsoDentroScene");
+        DilmaDentroScene = PlayerPrefs.GetInt("DilmaDentroScene");
+        SuplicyDentroScene = PlayerPrefs.GetInt("SuplicyDentroScene");
 
         if (LulaDentroScene == 1)
         {
@@ -21,31 +29,163 @@ public class Partido : MonoBehaviour {
         {
             AtivaBolsonaro();
         }
-         PersonagensScene[0] = LulaScene;         PersonagensScene[1] = CiroScene;         PersonagensScene[2] = BolsonaroScene;       } 
-    // LULA      public void AtivaLula()     {         int i = 0;         PersonagensScene[i] = LulaScene;         PersonagensScene[i].SetActive(true);
+
+        if (DilmaDentroScene == 1)
+        {
+            AtivaDilma();
+        }
+
+        if (SuplicyDentroScene == 1)
+        {
+            AtivaSuplicy();
+        }
+
+        if (EneasDentroScene == 1)
+        {
+            AtivaEneas();
+        }
+
+        PersonagensScene[0] = LulaScene;
+        PersonagensScene[1] = CiroScene;
+        PersonagensScene[2] = BolsonaroScene;
+        PersonagensScene[3] = DilmaScene;
+        PersonagensScene[4] = SuplicyScene;
+        PersonagensScene[5] = EneasScene;
+
+
+    }
+
+    // LULA
+
+    public void AtivaLula()
+    {
+        int i = 0;
+        PersonagensScene[i] = LulaScene;
+        PersonagensScene[i].SetActive(true);
         LulaDentroScene = 1;
-        PlayerPrefs.SetInt("LulaDentroScene", LulaDentroScene);      }      public void DesativaLula()     {         PersonagensScene[0].SetActive(false);
+        PlayerPrefs.SetInt("LulaDentroScene", LulaDentroScene);
+
+    }
+
+    public void DesativaLula()
+    {
+        PersonagensScene[0].SetActive(false);
         LulaDentroScene = 0;
         PlayerPrefs.SetInt("LulaDentroScene", LulaDentroScene);
-     }
+
+    }
 
     // ------------------------------------------------------------------ //
-     // CIRO
 
-     public void AtivaCiro()     {         int i = 1;         PersonagensScene[i] = CiroScene;         PersonagensScene[i].SetActive(true);         CiroDentroScene = 1;
-        PlayerPrefs.SetInt("CiroDentroScene", CiroDentroScene);      }      public void DesativaCiro()     {          PersonagensScene[1].SetActive(false);         CiroDentroScene = 0;
-        PlayerPrefs.SetInt("CiroDentroScene", CiroDentroScene);      }
-     // ------------------------------------------------------------------ //
+    // CIRO
+
+
+    public void AtivaCiro()
+    {
+        int i = 1;
+        PersonagensScene[i] = CiroScene;
+        PersonagensScene[i].SetActive(true);
+        CiroDentroScene = 1;
+        PlayerPrefs.SetInt("CiroDentroScene", CiroDentroScene);
+
+    }
+
+    public void DesativaCiro()
+    {
+
+        PersonagensScene[1].SetActive(false);
+        CiroDentroScene = 0;
+        PlayerPrefs.SetInt("CiroDentroScene", CiroDentroScene);
+
+    }
+
+    // ------------------------------------------------------------------ //
 
 
     // BOLSONARO
-     public void AtivaBolsonaro()     {         int i = 2;         PersonagensScene[i] = BolsonaroScene;         PersonagensScene[i].SetActive(true);         BolsoDentroScene = 1;
-        PlayerPrefs.SetInt("BolsoDentroScene", BolsoDentroScene);      }      public void DesativaBolsonaro()     {         PersonagensScene[2].SetActive(false);         BolsoDentroScene = 0;
-        PlayerPrefs.SetInt("BolsoDentroScene", BolsoDentroScene);      }
+
+    public void AtivaBolsonaro()
+    {
+        int i = 2;
+        PersonagensScene[i] = BolsonaroScene;
+        PersonagensScene[i].SetActive(true);
+        BolsoDentroScene = 1;
+        PlayerPrefs.SetInt("BolsoDentroScene", BolsoDentroScene);
+
+    }
+
+    public void DesativaBolsonaro()
+    {
+        PersonagensScene[2].SetActive(false);
+        BolsoDentroScene = 0;
+        PlayerPrefs.SetInt("BolsoDentroScene", BolsoDentroScene);
+
+    }
+
+    // DILMA
+
+    public void AtivaDilma()
+    {
+        int i = 3;
+        PersonagensScene[i] = DilmaScene;
+        PersonagensScene[i].SetActive(true);
+        DilmaDentroScene = 1;
+        PlayerPrefs.SetInt("DilmaDentroScene", DilmaDentroScene);
+
+    }
+
+    public void DesativaDilma()
+    {
+        PersonagensScene[3].SetActive(false);
+        DilmaDentroScene = 0;
+        PlayerPrefs.SetInt("DilmaDentroScene", DilmaDentroScene);
+
+    }
+
+    // SUPLICY
+
+    public void AtivaSuplicy()
+    {
+        int i = 4;
+        PersonagensScene[i] = SuplicyScene;
+        PersonagensScene[i].SetActive(true);
+        SuplicyDentroScene = 1;
+        PlayerPrefs.SetInt("SuplicyDentroScene", SuplicyDentroScene);
+
+    }
+
+    public void DesativaSuplicy()
+    {
+        PersonagensScene[4].SetActive(false);
+        SuplicyDentroScene = 0;
+        PlayerPrefs.SetInt("SuplicyDentroScene", SuplicyDentroScene);
+
+    }
+
+    // ENEAS
+
+    public void AtivaEneas()
+    {
+        int i = 5;
+        PersonagensScene[i] = EneasScene;
+        PersonagensScene[i].SetActive(true);
+        EneasDentroScene = 1;
+        PlayerPrefs.SetInt("EneasDentroScene", EneasDentroScene);
+
+    }
+
+    public void DesativaEneas()
+    {
+        PersonagensScene[5].SetActive(false);
+        EneasDentroScene = 0;
+        PlayerPrefs.SetInt("EneasDentroScene", EneasDentroScene);
+
+
+    }
 
 
 
+}
 
-} 
 
 
