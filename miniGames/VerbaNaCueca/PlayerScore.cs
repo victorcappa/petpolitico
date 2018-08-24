297a94ea41d6c6eb5c521b40768e282ec8b52161
victorcappa <40408965+victorcappa@users.noreply.github.com>
2018-08-24 12:57:55 -0300

    Update PlayerScore.cs
    Adicionado:
    
    mais inimigos;
    power up habeas corpus;
    dificuldade aumenta conforme pega verba;
    sons;

--- miniGames/VerbaNaCueca/PlayerScore.cs
+++ miniGames/VerbaNaCueca/PlayerScore.cs
@@ -1,45 +1,147 @@
-﻿
+
 using UnityEngine;
 using UnityEngine.UI;
-using UnityEngine.SceneManagement;
-using System.Collections;
-public class PlayerScore : MonoBehaviour {
+
+public class PlayerScore : MonoBehaviour
+{
 
     private Text scoreText;
-    private int score = 0;
-
-
-    private void Awake()
+    private int score = 10;
+    int chances;
+    public Text chances_txtUI;
+    public GameObject telaOver;
+    public int quantoGanhou;
+    public Text quantoGanhouTxt;
+    public AudioSource SomVerba, SomPf, GameOver, SomPowerUp;
+    public bool habeasCorpus;
+    public GameObject HabeasCorpus;
+    public int qteHabeasCorpus;
+    public Text qteHabeasCorpusTxt;
+    public void Awake()
     {
-        scoreText = GameObject.Find("Verba_txt").GetComponent<Text>();
-        score = PlayerPrefs.GetInt("Verba");
-        scoreText.text = score.ToString();
+       // scoreText = GameObject.Find("Verba_txt").GetComponent<Text>();
+       // scoreText.text = PlayerPrefs.GetInt("Verba").ToString();
+        chances = 3;
+        chances_txtUI.text = chances.ToString();
+
 
     }
+
+    public void Update()
+    {
+        
+    }
+
 
     private void OnTriggerEnter2D(Collider2D target)
     {
-        if (target.tag == "PF")
+        if (target.tag == "PF" || target.tag == "Moro" || target.tag == "Receita")
         {
-            transform.position = new Vector2(0,100); // mover o player pra longe (pra simular que ele sumiu / se sumir de verdade, o script nao roda)
             target.gameObject.SetActive(false);
-            StartCoroutine(RestartGame());
+
+            if (habeasCorpus == true)
+            {
+                chances = chances + 1;
+                qteHabeasCorpus = qteHabeasCorpus - 1;
+
+                qteHabeasCorpusTxt.text = qteHabeasCorpus.ToString();
+
+                if (qteHabeasCorpus == 0)
+                {
+                    habeasCorpus = false;
+                    HabeasCorpus.gameObject.SetActive(false);
+
+                }
+            
+               
+
+
+            }
+
+           
+                chances = chances - 1;
+
+                chances_txtUI.text = chances.ToString();
+                SomPf.Play();
+
+
+          
+
+            if (chances <= 0 && habeasCorpus == false)
+            {
+                telaOver.gameObject.SetActive(true);
+
+                this.gameObject.GetComponent<Image>().enabled = false;
+
+                //transform.position = new Vector2(0, 100); // mover o player pra longe (pra simular que ele sumiu / se sumir de verdade, o script nao roda)
+
+                PlayerPrefs.SetInt("Chances", chances);
+
+                GameOver.Play();
+                SomPf.Play();
+
+
+
+            }
+
         }
 
-        if (target.tag ==  "Verba_MiniGame")
+        if (target.tag == "Verba_MiniGame")
+        {
+            score = 5;
+            target.gameObject.SetActive(false);
+            scoreText = GameObject.Find("Verba_txt").GetComponent<Text>();
+            PlayerPrefs.SetInt("Verba", PlayerPrefs.GetInt("Verba") + score);
+            scoreText.text = PlayerPrefs.GetInt("Verba").ToString();
+            quantoGanhou = quantoGanhou + score;
+            quantoGanhouTxt.text = quantoGanhou.ToString();
+            SomVerba.Play();
+            PlayerPrefs.SetInt("ScoreCueca", score);
+        }
+
+        if (target.tag == "HabeasCorpus")
         {
             target.gameObject.SetActive(false);
-            score = score + 10;
-            scoreText.text = score.ToString();
+            habeasCorpus = true;
+            HabeasCorpus.gameObject.SetActive(true);
+            SomPowerUp.Play();
+            qteHabeasCorpus = qteHabeasCorpus + 1;
+            qteHabeasCorpusTxt.text = qteHabeasCorpus.ToString();
+
         }
+
+
     }
 
-    IEnumerator RestartGame()
+   
+
+    public void RestartGame()
     {
-        yield return new WaitForSecondsRealtime(2f);
-        SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+        telaOver.gameObject.SetActive(false);
+
+        PlayerPrefs.SetInt("ScoreCueca", 0);
+
+        scoreText = GameObject.Find("Verba_txt").GetComponent<Text>();
+        scoreText.text = PlayerPrefs.GetInt("Verba").ToString();
+
+        chances = 3;
+        chances_txtUI.text = chances.ToString();
+        PlayerPrefs.SetInt("Chances", chances);
+       
+        GameOver.Stop();
+
+        habeasCorpus = false;
+       // SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+        this.gameObject.GetComponent<Image>().enabled = true;
+
+        // transform.position = new Vector2(0, 0);
+
     }
 
+   
+
+
+  
 
 
 }// FIM DA CLASSE
