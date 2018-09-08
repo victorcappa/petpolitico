4a4035f66f1d63dad29796ece4a65af7e64f76ac
victorcappa <40408965+victorcappa@users.noreply.github.com>
2018-09-08 20:10:09 -0300

    Update FichaLimpa.cs

--- status/FichaLimpa.cs
+++ status/FichaLimpa.cs
@@ -45,7 +45,7 @@ public class FichaLimpa : MonoBehaviour {
         FichaPersonagens();
         CPI();
 
-        GanhaFicha();
+       // GanhaFicha();
         MaxMinFicha();
 
         AlmaHonesta();
@@ -82,11 +82,11 @@ public class FichaLimpa : MonoBehaviour {
     public void FichaPersonagens()
 
     {
-
+        veloPerdaFicha = 0.05f;
         if (PlayerPrefs.GetInt("LulaDentroScene") == 1)
 
         {
-
+            PlayerPrefs.SetFloat("FichaNaScene", PlayerPrefs.GetFloat("FichaLula"));
             fichaLimpa = PlayerPrefs.GetFloat("FichaLula");
             float tempFicha = Mathf.Round(PlayerPrefs.GetFloat("FichaLula"));
             ficha_txtUI.text = (tempFicha.ToString()); // + "%");
@@ -129,6 +129,8 @@ public class FichaLimpa : MonoBehaviour {
 
         if (PlayerPrefs.GetInt("CiroDentroScene") == 1)
         {
+            PlayerPrefs.SetFloat("FichaNaScene", PlayerPrefs.GetFloat("FichaCiro"));
+
             fichaLimpa = PlayerPrefs.GetFloat("FichaCiro");
             float tempFicha = Mathf.Round(PlayerPrefs.GetFloat("FichaCiro"));
             ficha_txtUI.text = (tempFicha.ToString()); // + "%");
@@ -166,6 +168,9 @@ public class FichaLimpa : MonoBehaviour {
 
         if (PlayerPrefs.GetInt("BolsoDentroScene") == 1)
         {
+
+            PlayerPrefs.SetFloat("FichaNaScene", PlayerPrefs.GetFloat("FichaBolso"));
+
             fichaLimpa = PlayerPrefs.GetFloat("FichaBolso");
             float tempFicha = Mathf.Round(PlayerPrefs.GetFloat("FichaBolso"));
             ficha_txtUI.text = (tempFicha.ToString()); // + "%");
@@ -200,6 +205,9 @@ public class FichaLimpa : MonoBehaviour {
 
         if (PlayerPrefs.GetInt("DilmaDentroScene") == 1)
         {
+
+            PlayerPrefs.SetFloat("FichaNaScene", PlayerPrefs.GetFloat("FichaDilma"));
+
             fichaLimpa = PlayerPrefs.GetFloat("FichaDilma");
             float tempFicha = Mathf.Round(PlayerPrefs.GetFloat("FichaDilma"));
             ficha_txtUI.text = (tempFicha.ToString()); // + "%");
@@ -234,6 +242,9 @@ public class FichaLimpa : MonoBehaviour {
 
         if (PlayerPrefs.GetInt("SuplicyDentroScene") == 1)
         {
+            
+            PlayerPrefs.SetFloat("FichaNaScene", PlayerPrefs.GetFloat("FichaSuplicy"));
+
             fichaLimpa = PlayerPrefs.GetFloat("FichaSuplicy");
             float tempFicha = Mathf.Round(PlayerPrefs.GetFloat("FichaSuplicy"));
             ficha_txtUI.text = (tempFicha.ToString()); //) + "%");
@@ -269,6 +280,8 @@ public class FichaLimpa : MonoBehaviour {
 
         if (PlayerPrefs.GetInt("EneasDentroScene") == 1)
         {
+            PlayerPrefs.SetFloat("FichaNaScene", PlayerPrefs.GetFloat("FichaEneas"));
+
             fichaLimpa = PlayerPrefs.GetFloat("FichaEneas");
             float tempFicha = Mathf.Round(PlayerPrefs.GetFloat("FichaEneas"));
             ficha_txtUI.text = (tempFicha.ToString());// + "%");
@@ -301,25 +314,161 @@ public class FichaLimpa : MonoBehaviour {
 
         }
     
-    }
+        if (PlayerPrefs.GetInt("DodorioDentroScene") == 1)
 
+        {
+            PlayerPrefs.SetFloat("FichaNaScene", PlayerPrefs.GetFloat("FichaDodorio"));
+
+            fichaLimpa = PlayerPrefs.GetFloat("FichaDodorio");
+            float tempFicha = Mathf.Round(PlayerPrefs.GetFloat("FichaDodorio"));
+            ficha_txtUI.text = (tempFicha.ToString()); // + "%");
+
+            fichaLimpa = PlayerPrefs.GetFloat("FichaDodorio");
+            fichaLimpa -= Time.deltaTime * veloPerdaFicha;
+            PlayerPrefs.SetFloat("FichaDodorio", fichaLimpa);
+
+            // BARRA Ficha
+
+            barraFicha.maxValue = max_ficha;
+            barraFicha.value = fichaLimpa;
+
+            //
+
+            //MIN E MAX
+
+            if (PlayerPrefs.GetFloat("FichaDodorio") <= 0)
+            {
+                PlayerPrefs.SetFloat("FichaDodorio", 0);
+            }
+
+            if (PlayerPrefs.GetFloat("FichaDodorio") >= 100)
+            {
+                PlayerPrefs.SetFloat("FichaDodorio", 100);
+            }
+
+
+            // INICIO DO JOGO
+            if (PlayerPrefs.GetInt("LevelDodorio") <= 1 && PlayerPrefs.GetFloat("FichaDodorio") <= 100 && PlayerPrefs.GetFloat("PopularidadeDodorio") <= 50)
+            {
+                PlayerPrefs.SetFloat("FichaDodorio", 100);
+            }
+
+
+
+
+        }
+    
+        if (PlayerPrefs.GetInt("HaddardDentroScene") == 1)
+
+        {
+            PlayerPrefs.SetFloat("FichaNaScene", PlayerPrefs.GetFloat("FichaHaddard"));
+
+            fichaLimpa = PlayerPrefs.GetFloat("FichaHaddard");
+            float tempFicha = Mathf.Round(PlayerPrefs.GetFloat("FichaHaddard"));
+            ficha_txtUI.text = (tempFicha.ToString()); // + "%");
+
+            fichaLimpa = PlayerPrefs.GetFloat("FichaHaddard");
+            fichaLimpa -= Time.deltaTime * veloPerdaFicha;
+            PlayerPrefs.SetFloat("FichaHaddard", fichaLimpa);
+
+            // BARRA Ficha
+
+            barraFicha.maxValue = max_ficha;
+            barraFicha.value = fichaLimpa;
+
+            //
+
+            //MIN E MAX
+
+            if (PlayerPrefs.GetFloat("FichaHaddard") <= 0)
+            {
+                PlayerPrefs.SetFloat("FichaHaddard", 0);
+            }
+
+            if (PlayerPrefs.GetFloat("FichaHaddard") >= 100)
+            {
+                PlayerPrefs.SetFloat("FichaHaddard", 100);
+            }
+
+
+            // INICIO DO JOGO
+            if (PlayerPrefs.GetInt("LevelHaddard") <= 1 && PlayerPrefs.GetFloat("FichaHaddard") <= 100 && PlayerPrefs.GetFloat("PopularidadeHaddard") <= 50)
+            {
+                PlayerPrefs.SetFloat("FichaHaddard", 100);
+            }
+
+
+
+
+        }
+    
+        if (PlayerPrefs.GetInt("CunhaDentroScene") == 1)
+
+        {
+            PlayerPrefs.SetFloat("FichaNaScene", PlayerPrefs.GetFloat("FichaCunha"));
+
+            fichaLimpa = PlayerPrefs.GetFloat("FichaCunha");
+            float tempFicha = Mathf.Round(PlayerPrefs.GetFloat("FichaCunha"));
+            ficha_txtUI.text = (tempFicha.ToString()); // + "%");
+
+            fichaLimpa = PlayerPrefs.GetFloat("FichaCunha");
+            fichaLimpa -= Time.deltaTime * veloPerdaFicha;
+            PlayerPrefs.SetFloat("FichaCunha", fichaLimpa);
+
+            // BARRA Ficha
+
+            barraFicha.maxValue = max_ficha;
+            barraFicha.value = fichaLimpa;
+
+            //
+
+            //MIN E MAX
+
+            if (PlayerPrefs.GetFloat("FichaCunha") <= 0)
+            {
+                PlayerPrefs.SetFloat("FichaCunha", 0);
+            }
+
+            if (PlayerPrefs.GetFloat("FichaCunha") >= 100)
+            {
+                PlayerPrefs.SetFloat("FichaCunha", 100);
+            }
+
+
+            // INICIO DO JOGO
+            if (PlayerPrefs.GetInt("LevelCunha") <= 1 && PlayerPrefs.GetFloat("FichaCunha") <= 100 && PlayerPrefs.GetFloat("PopularidadeCunha") <= 50)
+            {
+                PlayerPrefs.SetFloat("FichaCunha", 100);
+            }
+
+
+
+
+        }
+    }
 
     // METODO DE PERDA DE POPULARIDADE COM FICHA LIMPA BAIXA
     public void CPI()
 
     {
-        if (fichaLimpa <= 20)
+        if (fichaLimpa <= 30)
         {
-            PlayerPrefs.SetFloat("VeloPerdaPop", 0.5f);
+            GetComponent<Popularidade>().veloPerdaPop = 0.1f;
+
+            //PlayerPrefs.SetFloat("VeloPerdaPop", 0.1f);
 
             cpi = 1;
             PlayerPrefs.SetInt("CPI", cpi);
-          
+
+            PlayerPrefs.SetInt("AlertaFichaBaixo", 1);
+
         }
 
-        if (fichaLimpa > 20)
+        if (fichaLimpa > 30)
         {
-            PlayerPrefs.SetFloat("VeloPerdaPop", 0.1f);
+           GetComponent<Popularidade>().veloPerdaPop = 0.07f;
+
+           // PlayerPrefs.SetFloat("VeloPerdaPop", 0.07f);
 
             cpi = 0;
 
@@ -329,6 +478,7 @@ public class FichaLimpa : MonoBehaviour {
         }
 
         PlayerPrefs.SetFloat("VeloPerdaPop", PlayerPrefs.GetFloat("VeloPerdaPop"));
+        PlayerPrefs.SetInt("AlertaFichaBaixo", 0);
 
 
 
@@ -341,7 +491,7 @@ public class FichaLimpa : MonoBehaviour {
         {
             if (PlayerPrefs.GetFloat("PopularidadeNaScene") < 51)
             {
-                PlayerPrefs.SetFloat("VeloGanhaPop", 0.5f);
+                PlayerPrefs.SetFloat("VeloGanhaPop", 0.2f);
 
             }
             if (PlayerPrefs.GetFloat("PopularidadeNaScene") >= 51)
@@ -352,13 +502,16 @@ public class FichaLimpa : MonoBehaviour {
 
             almaHonesta = 1;
             PlayerPrefs.SetInt("AlmaHonesta", almaHonesta);
+            PlayerPrefs.SetInt("AlertaFichaAlto", 1);
+
 
         }
-        if (fichaLimpa < 90 && fichaLimpa > 20)
+        if (fichaLimpa < 90 && fichaLimpa > 30)
         {
             PlayerPrefs.SetFloat("VeloGanhaPop", 0);
             almaHonesta = 0;
             PlayerPrefs.SetInt("AlmaHonesta", almaHonesta);
+            PlayerPrefs.SetInt("AlertaFichaAlto", 0);
 
         }
 
@@ -371,3 +524,5 @@ public class FichaLimpa : MonoBehaviour {
 
     }
 }
+
+
